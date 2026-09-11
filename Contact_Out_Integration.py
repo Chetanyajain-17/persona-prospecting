6c67364d7ebd2b3ee2a6ca7485fab8a03fcdfc23
Chetanyajain-17 <chetanyajain40706@gmail.com>
2026-09-11 12:50:05 +0530

    Update Contact_Out_Integration.py

--- Contact_Out_Integration.py
+++ Contact_Out_Integration.py
@@ -3689,15 +3689,7 @@ def render_bulk_processing():
         "performed during bulk company processing."
     )
 
-    current_signature = (
-
-        f"{uploaded.name}:"
-        f"{len(base_df)}:"
-        f"{','.join("
-        f"base_df['Input Company']"
-        f".astype(str)"
-        f".tolist()[:20])}"
-    )
+   current_signature = ( f"{uploaded.name}:" f"{len(base_df)}:" f"{','.join(base_df['Input Company'].astype(str).tolist()[:20])}" )
 
     if (
         st.session_state.get(
